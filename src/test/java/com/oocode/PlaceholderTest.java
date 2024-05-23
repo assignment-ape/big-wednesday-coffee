7477d41d61797ab4c378bc003998262d4b1012c5
assignment-ape <assignment_3887847932011@hotmail.com>
2024-05-24 00:09:29 +0100

    Added testCreatePageWithValidData to check date filtering with previous 3 days - TDD practice

--- src/test/java/com/oocode/PlaceholderTest.java
+++ src/test/java/com/oocode/PlaceholderTest.java
@@ -1,13 +1,34 @@
 package com.oocode;
-
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 
 public class PlaceholderTest {
+
     @Test
-    public void doesNothing() throws Exception {
-        assertThat(1 + 1, equalTo(2));
+    public void testCreatePageWithValidData() throws Exception {
+
+        // path of CSV test data (valid data)
+        String filePath = "src/test/resources/data_three_days_ago.csv";
+        String url = "file://" + new File(filePath).getAbsolutePath();
+
+        // call create page method with valid data and date
+        LocalDate testDate = LocalDate.of(2024, 5, 12);
+        Main.createPage(url, testDate);
+
+        // read index.html
+        List<String> lines = Files.readAllLines(Path.of("index.html"));
+        String content = String.join("\n", lines);
+
+        // check HTML content contains the expected string
+        String expected_string =  "You should have been at Caloundra on Thursday - it was gnarly - waves up to 1.960m!";
+        assertThat(content, containsString(expected_string));
     }
 }
