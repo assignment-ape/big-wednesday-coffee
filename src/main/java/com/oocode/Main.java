fb28ad468fa8c3d7ddaea3d1d758cd1fd8510713
assignment-ape <assignment_3887847932011@hotmail.com>
2024-05-26 16:15:03 +0100

    Added main with HttpClient to make CI/CD pipeline

--- src/main/java/com/oocode/Main.java
+++ src/main/java/com/oocode/Main.java
@@ -8,6 +8,7 @@ import okhttp3.ResponseBody;
 
 import java.io.*;
 import java.net.InetSocketAddress;
+import java.net.http.HttpClient;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -26,23 +27,29 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
 
 public class Main {
+  private final HttpClient httpClient;
+
+  public Main(HttpClient httpClient) {
+    this.httpClient = httpClient;
+  }
+
   public static void main(String[] args) throws Exception {
     String url = args.length == 2 ? args[0] : "https://apps.des.qld.gov.au/data-sets/waves/wave-7dayopdata.csv";
     LocalDate today = args.length == 2 ? LocalDate.parse(args[1]) : LocalDate.now();
 
     createPage(url, today);
-    createAndStartServer();
+    startServer();
   }
 
   public static void createPage(String url, LocalDate today) throws Exception {
     List<String[]> result;
     if (url.startsWith("file:")) {
-      // add URL as file path
+      // Local file path
       try (CSVReader reader = new CSVReader(new FileReader(url.substring(5)))) {
         result = reader.readAll().stream().skip(2).collect(toList());
       }
     } else {
-      // If the URL is a remote URL
+      // Remote URL
       try (Response r = new OkHttpClient().newCall(new Request.Builder().url(url).build()).execute()) {
         if (r.isSuccessful()) {
           try (ResponseBody rb = r.body()) {
@@ -56,11 +63,11 @@ public class Main {
       }
     }
 
-    // find three days ago at the start of the day
+    // Find three days ago at the start of the day
     LocalDateTime startOfDay = today.atStartOfDay();
     LocalDateTime threeDaysAgoOfStart = startOfDay.minusDays(3);
 
-    // check if the parsed dateTime is after threeDaysAgoOfStart and before startOfDay
+    // Filter data
     List<String[]> filterDate = result.stream()
             .filter(o -> {
               LocalDateTime dateTime = LocalDateTime.ofEpochSecond(parseInt(o[2]), 0, ZoneOffset.ofHours(10));
@@ -74,15 +81,16 @@ public class Main {
 
     String[] strings = filterDate.stream().max(comparing(o -> Double.valueOf(o[7]))).orElse(null);
 
-    // define latitude and longitude
+    // Define latitude and longitude
     String latitude = strings[4];
     String longitude = strings[5];
     String mapTitle = strings[0];
 
-    // create Google Maps link
-    String googleMapsLink = String.format("<a style=\"color: #007bff; text-decoration: none; font-weight: bold;\"" +
-                    " class=\"map-link\" target=\"_blank\" href=\"https://www.google.com/maps/search/?api=1&query=%s,%s\">" +
-                    "Open Map of %s</a>", latitude, longitude, mapTitle);
+    // Create Google Maps link
+    String googleMapsLink = String.format(
+            "<a style=\"color: #007bff; text-decoration: none; font-weight: bold;\" class=\"map-link\" target=\"_blank\" " +
+                    "href=\"https://www.google.com/maps/search/?api=1&query=%s,%s\">Open Map of %s</a>",
+            latitude, longitude, mapTitle);
 
     try (FileWriter myWriter = new FileWriter("index.html")) {
       LocalDateTime foo = LocalDateTime.ofEpochSecond(parseInt(strings[2]), 0, ZoneOffset.ofHours(10));
@@ -92,7 +100,7 @@ public class Main {
     }
   }
 
-  public static void createAndStartServer() throws IOException {
+  public static void startServer() throws IOException {
     HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
     server.createContext("/", new HttpHandler() {
       @Override
